9e85e8ff2e52376e282689d537dfdc47deb3f827
Adam McElwee <adam@mcelwee.me>
2012-05-21 01:05:41 -0500

    Filenames parsed correctly. Correct episode result retrieved.

--- main.js
+++ main.js
@@ -1,120 +1,86 @@
-var argv = require('optimist').demand('f').argv,
-    apiKey = 'FDA2EF936F4B9366',
-    baseMirrorPath = 'http://thetvdb.com',
-    Shred = require('shred'),
-    shred = new Shred(),
-    sprintf = require('sprintf').sprintf,
-    xml2js = require('xml2js');
+var optimist = require('optimist'),
+    apiKey = '3F9F02EE949DBFC4',
+    exec = require('child_process').exec,
+    Q = require('q'),
+    shows = {},
+    episodesByShow = {},
+    tvdb = new (require('node-tvdb'))({ apiKey : apiKey }),
+    tvShowRegex = /(.*)[sS]\.?(\d{2})\.?[eE]\.?(\d{2}).*/,
+    _ = require('underscore')._,
+    _s = require('underscore.string'),
+    argv = optimist.argv,
+    regexMatch;
 
-/**
- * Pull down latest mirrors for TheTVDB
- * @param callback
- */
-function retrieveMirrors( callback ) {
-    var mirrorsUrlPattern = 'http://www.thetvdb.com/api/%s/mirrors.xml',
-        mirrorsUrl = sprintf(mirrorsUrlPattern, apiKey);
+regexMatch = tvShowRegex.exec( argv.s );
+
+function findShowInfo( showName ) {
+    var deferred = Q.defer();
     
-    shred.get({
-        url : mirrorsUrl,
-        on : {
-            200 : function(response) {
-                parseXml(response.content.body, function(result) {
-                    callback(result);
-                });
+    if ( !shows[ showName ] ) {
+        tvdb.findTvShow( showName, function(err, response ) {
+            var match = _.find( response, function( show ) {
+                return showName && show.name.toLowerCase() === showName.toLowerCase();
+            });
+
+            if ( match ) {
+                shows[ showName ] = match;
+                deferred.resolve( match );
             }
-        }
-    })
+        });
+    } else {
+        deferred.resolve( shows[ showName ] );
+    }
+
+    return deferred.promise;
 }
 
-function parseXml(xmlDataStream, callback) {
-    var parser = new xml2js.Parser();
+function findEpisodeInfo( show ) {
+    var deferred = Q.defer();
 
-    parser.on('end', function(result) {
-        callback(result);
+    if ( episodesByShow[ show.id ] ) {
+        deferred.resolve( episodesByShow[ show.id ] );
+    } else {
+        tvdb.getInfo( null, show.id, function( err, response ) {
+
+            response = processEpisodesResponse( response );
+            episodesByShow[ show.id ] = response;
+            deferred.resolve( response );
+        });
+    }
+
+    return deferred.promise;
+}
+
+function processEpisodesResponse( response ) {
+    var result = {};
+    
+    _.each( response, function( episode ) {
+        var season = result[ episode.SeasonNumber ] || {};
+        season[ episode.EpisodeNumber ] = episode;
+        result[ episode.SeasonNumber ] = season;
     });
 
-    parser.parseString(xmlDataStream);
+    return result;
 }
 
-function retrieveServerTime() {
-    var url = 'http://www.thetvdb.com/api/Updates.php?type=none';
+if ( regexMatch ) {
+    findShowInfo( _s.clean( regexMatch[ 1 ].replace(/\./g, ' ') ) )
+        .then( function( show ) {
+            return findEpisodeInfo( show );
+        })
+        .then( function( episodes ) {
+            var seasonNumber = _s.toNumber( regexMatch[ 2 ] ).toString(),
+                episodeNumber = _s.toNumber( regexMatch[ 3 ] ).toString(),
+                season = episodes[ seasonNumber ],
+                episode = season && season[ episodeNumber ];
 
-    shred.get({
-        url : url,
-        on : {
-            200 : function(response) {
-                parseXml(response.content.body, function(result) {
-                    console.log(result);
-                });
-            }
-        }
-    })
+            console.log( episode );
+
+//            console.log( foo );
+//            function puts(error, stdout, stderr) {
+//                console.log(stderr);
+//                console.log(stdout);
+//            }
+//            exec('AtomicParsley', puts);
+        });
 }
-
-function getSeries(name, callback) {
-    var baseUrl = 'http://www.thetvdb.com/api/GetSeries.php?seriesname=';
-
-    shred.get({
-        url : baseUrl + encodeURIComponent(name),
-        on : {
-            200 : function(response) {
-                parseXml(response.content.body, function(result) {
-                    if (result.Series.length && result.Series[0].SeriesName === name) {
-                        console.log(result.Series[0]);
-                        callback(result.Series[0].seriesid);
-                    } else {
-                        callback();
-                    }
-                });
-            }
-        }
-    });
-}
-
-function getSeriesInfo(seriesId, callback) {
-    var seriesInfoUrl = sprintf('%s/api/%s/series/%s/all/en.xml', baseMirrorPath, apiKey, seriesId);
-
-    shred.get({
-        url : seriesInfoUrl,
-        on : {
-            200 : function(response) {
-                parseXml(response.content.body, function(result) {
-                    callback(result);
-                })
-            }
-        }
-    });
-}
-
-function getBanners(seriesId, callback) {
-    var bannersUrl = sprintf('%s/api/%s/series/%s/banners.xml', baseMirrorPath, apiKey, seriesId);
-
-    shred.get({
-        url : bannersUrl,
-        on : {
-            200 : function(response) {
-                parseXml(response.content.body, function(result) {
-                    callback(result);
-                });
-            }
-        }
-    })
-}
-
-if (argv.m) {
-    retrieveMirrors(function(url) {
-        console.log(url);
-    });
-}
-
-retrieveServerTime();
-
-getSeries("Dexter", function(seriesId) {
-    console.log(seriesId);
-//    getSeriesInfo(seriesId, function(response) {
-//        console.log(response);
-//    });
-    getBanners(seriesId, function(result) {
-        console.log(result);
-    });
-});
